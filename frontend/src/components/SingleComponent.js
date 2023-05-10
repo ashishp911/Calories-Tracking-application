210bbbee4fa05a21549e78627e96f23277131d95
ashishp911 <ashish.patil35@gmail.com>
2023-05-10 13:30:12 -0500

    started entriesComponent

--- frontend/src/components/SingleComponent.js
+++ frontend/src/components/SingleComponent.js
@@ -1,31 +1,67 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
-import { Row, Button, Card, Col } from 'react-bootstrap'
+import React from "react";
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Row, Button, Card, Col } from "react-bootstrap";
 
 const SingleComponent = ({entryData, setChangeIngredient, deleteSingleEntry, setChangeEntry}) => {
+  
+    function changeIngredient() {
+    setChangeIngredient({
+      "change": true,
+      "id": entryData._id,
+    });
+  }
+  function changeEntry() {
+    setChangeEntry(
+        {
+            "change": true,
+            "id": entryData._id,
+        }
+    )
+  }
+
   return (
     <Card>
-        <Row>
-            <Col>
-                Dish : {entryData != undefined && entryData.dish}
-            </Col>
-            <Col>
-                Ingredients : {entryData != undefined && entryData.ingredients}
-            </Col>
-            <Col>
-                Calories : {entryData != undefined && entryData.calories}
-            </Col>
-            <Col>
-                Fat : {entryData != undefined && entryData.fat}
-            </Col>
-
-            <Col><Button onClick={}> Delete Entry </Button></Col>
-            <Col><Button onClick={}> Change Ingredients </Button></Col>
-            <Col><Button onClick={}> Change Entry </Button></Col>
-        </Row>
+      <Row>
+        <Col>Dish : {entryData != undefined && entryData.dish}</Col>""
+        <Col>
+          Ingredients : {entryData != undefined && entryData.ingredients}
+        </Col>
+        <Col>Calories : {entryData != undefined && entryData.calories}</Col>
+        <Col>Fat : {entryData != undefined && entryData.fat}</Col>
+        <Col>
+          <Button
+            onClick={() => {
+              deleteSingleEntry(entryData._id);
+            }}
+          >
+            {" "}
+            Delete Entry{" "}
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            onClick={() => {
+              changeIngredient();
+            }}
+          >
+            {" "}
+            Change Ingredients{" "}
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            onClick={() => {
+              changeEntry();
+            }}
+          >
+            {" "}
+            Change Entry{" "}
+          </Button>
+        </Col>
+      </Row>
     </Card>
-  )
-}
+  );
+};
 
-export default SingleComponent
+export default SingleComponent;
